27add42c214b40b9b6d842f7ebfc57b7420df9ab
toshiyuki <t.toshi.0412@gmail.com>
2013-02-22 23:29:28 +0900

    Configuration for publishing

--- project/PlayjodaroutesbinderBuild.scala
+++ project/PlayjodaroutesbinderBuild.scala
@@ -20,6 +20,42 @@ object PlayjodaroutesbinderBuild extends Build {
         "play" %% "play-test" % "2.1.0" % "test",
         "org.scalatest" %% "scalatest" % "1.9.1" % "test"
       )
-    )
+    ) ++ publishingSettings
   )
+
+  val publishingSettings = Seq(
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) => _publishTo(v) },
+    publishArtifact in Test := false,
+    pomExtra := _pomExtra
+  )
+
+  def _publishTo(v: String) = {
+    val nexus = "https://oss.sonatype.org/"
+    if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
+    else Some("releases" at nexus + "service/local/staging/deploy/maven2")
+  }
+
+  val _pomExtra =
+    <url>http://github.com/tototoshi/play-joda-routes-binder</url>
+    <licenses>
+      <license>
+        <name>Apache License, Version 2.0</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+    <scm>
+      <url>git@github.com:tototoshi/play-joda-routes-binder.git</url>
+      <connection>scm:git:git@github.com:tototoshi/play-joda-routes-binder.git</connection>
+    </scm>
+    <developers>
+      <developer>
+        <id>tototoshi</id>
+        <name>Toshiyuki Takahashi</name>
+        <url>http://tototoshi.github.com</url>
+      </developer>
+    </developers>
+
+
 }
