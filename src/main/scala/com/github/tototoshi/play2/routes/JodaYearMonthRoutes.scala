5c1c978d7afc871ca5d1d2a361372a9bf3208969
toshiyuki <t.toshi.0412@gmail.com>
2013-06-13 21:13:56 +0900

    Update Copyright

--- src/main/scala/com/github/tototoshi/play2/routes/JodaYearMonthRoutes.scala
+++ src/main/scala/com/github/tototoshi/play2/routes/JodaYearMonthRoutes.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Toshiyuki Takahashi
+ * Copyright 2013 Toshiyuki Takahashi, Manabu Nakamura
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
